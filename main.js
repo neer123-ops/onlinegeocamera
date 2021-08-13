4bc48508d13a39093531ea0876b72fd4bac39722
neer123-ops <73882872+neer123-ops@users.noreply.github.com>
2021-08-13 17:52:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function take_snapshot () {
+    save("img.png");
+}
+
+function draw() {
+    image(video, 230, 150, 220, 200);
+
+    fill(250, 0, 0);
+    stroke(250, 0, 0);
+    circle(50, 50, 80);
+    circle(50, 430, 80);
+    circle(590,50, 80);
+    circle(590, 430, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(590, 90, 20, 300);
+}
